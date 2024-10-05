0d5b3990d448261918c44ae250aff1410a669408
gs <63047065+giaosu0719@users.noreply.github.com>
2024-10-05 22:52:45 +0700

    fix moss

--- dmoj/config/local_settings.py
+++ dmoj/config/local_settings.py
@@ -15,7 +15,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', '0') == '1'  # Change to False once you are done with runserver testing.
 HOST = os.environ.get('HOST', 'localhost')
-MOSS_API_KEY = os.environ.get('MOSS_API_KEY', None)
+if os.environ.get('MOSS_API_KEY') is not None:
+    MOSS_API_KEY = int(os.environ.get('MOSS_API_KEY'))
+else:
+    MOSS_API_KEY = None
 
 # Uncomment and set to the domain names this site is intended to serve.
 # You must do this once you set DEBUG to False.
